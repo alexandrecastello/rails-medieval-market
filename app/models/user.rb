9e5ebdefd5660edc0d508bfc10db3045bd98396d
Alexandre Castello <aaca91@gmail.com>
2020-06-01 17:45:17 -0300

    User model re-created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  has_many :owned_products, source: :products 
-  has_many :orders
-  has_many :bought_products, through: :orders, source: :products
-  # has_one_attached :avatar #testar se :avatar funciona, se não usar :photo
-  
-  # validates
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -42,13 +42,18 @@ ActiveRecord::Schema.define(version: 2020_06_01_184756) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "username"
-    t.string "password"
-    t.string "email"
-    t.string "avatar"
+    t.integer "coins"
     t.string "store_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "orders", "products"
