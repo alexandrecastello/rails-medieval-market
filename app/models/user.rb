6dd30011a0ca13bafbd45e4934cc897b121e7d89
Alexandre Castello <aaca91@gmail.com>
2020-06-01 18:34:36 -0300

    Models fixed (including class_name and foreign_key); product index view created; and product controller finished.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :owned_products, source: :products
-  has_many :orders
+  has_many :owned_products, class_name: 'Product', foreign_key: 'product_id', dependent: :destroy
+  has_many :orders, dependent: :destroy
   has_many :bought_products, through: :orders, source: :products
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
