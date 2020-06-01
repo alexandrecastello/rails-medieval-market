08b15780910891f1d634539e55afdd1b682d794e
Edgar Pellizzon <edgpelizzon@gmail.com>
2020-06-01 18:39:13 -0300

    seeds added to populate project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seeds users
+# username
+# email
+# password
+# store_name
+# coins_qtt
+
+puts "Adding users to db"
+25.times do |user|
+  User.create!(
+    name: 
+    username: Faker::Internet.username
+    email: Faker::Internet.safe_email(name: 'Nancy')
+    encrypted_password: Faker::Internet.password(min_length: 10, max_length: 20, mix_case: true)
+    coins: rand(1..100)
+  )
+    puts "User #{i + 1} added."
+    15.times do |i|
+      Product.create!(
+        name: Faker::Food.spice
+        description: Faker::
+        quantity: rand(1..20)
+        category: "spice"
+      )
+    end
+    puts "product #{i + 1} to user."
+end
+puts "seed generated"
+
+# seeds products
+# name
+# description
+# qtt
+# category
+# coin_price
+# tradable_for
+# other_price
+# negotiable
+
+
+# seeds orders
+
