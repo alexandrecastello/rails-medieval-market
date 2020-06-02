906f6b29717aefa22a91a6bcbb991aed7f54f701
Alexandre Castello <aaca91@gmail.com>
2020-06-02 16:43:33 -0300

    pull from user controller + product show view updated + product new view created + added coin_price to products (seed)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Adding users to db"
+10.times do |user|
+ u = User.create!(
+    username: Faker::Internet.unique.username,
+    email: Faker::Internet.unique.safe_email,
+    password: "123456",
+    coins: rand(1..100)
+  )
+    puts "User #{user + 1} added."
+    5.times do |prod|
+      Product.create!(
+        name: Faker::Food.unique.spice,
+        description: "a simple spice",
+        quantity: rand(1..20),
+        category: "spice",
+        owner: u,
+        coin_price: rand(1..50)
+      )
+      puts "product #{prod + 1} to user."
+    end
+end
+puts "seed generated"
+
+# seeds products
+# name
+# description
+# qtt
+# category
+# coin_price
+# tradable_for
+# other_price
+# negotiable
+
+
+# seeds orders
+
